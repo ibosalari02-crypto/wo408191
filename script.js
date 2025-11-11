8b4c5a8f20c3388fd8dc21c69a419249431620bb
amir reza <ibosalari02@gmail.com>
2025-11-11 09:15:33 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const names = ["alice","bob","charlie","david","eve",];
+
+for (let i = 0; i < 5; i++){
+    console.log(names[i]);
+}
+
+const students = [
+  ["ali", "computer", 20] 
+  ["sara", "computer",15]
+  ["mahdi", "physics",18]         
+];
+for (let i = 0; i < students.length; i++) {
+    if (students[i][2] > 12)
+    {console.log("name: " + students[i][0] + ", field: " + students[i][1] + ", score: " + students[i][2]);
+    }
+    
+    
+}
+    
+
+
+
+
+
+ 
